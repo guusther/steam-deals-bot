89440f36173da545807eb0579d970f1e182d7631
gusbigc <gustavo.ribeiro@acad.ufsm.br>
2026-09-17 07:22:14 -0300

    v1.1 adicao de links de redirecionamento + conversao pra brl

--- bot/bot.py
+++ bot/bot.py
@@ -31,6 +31,13 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send('Pong!')
 
+async def cotacaoDolar():
+    url = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resposta:
+            dados = await resposta.json()
+            return float(dados['USDBRL']['bid'])
+
 @bot.command()
 async def buscar(ctx, *, nome_jogo):
     await ctx.send(f"Buscando dados do {nome_jogo}...")
@@ -49,21 +56,27 @@ async def buscar(ctx, *, nome_jogo):
 
     #segurança pro bot nao quebrar
     if isinstance(dados,list) and len(dados) > 0:
+
+        cotacao = await cotacaoDolar()
+
         for jogo in dados[:3]:
             nome = jogo['external']
-            preco = jogo['cheapest']
+            precoUSD = float(jogo['cheapest'])
+            precoBRL = precoUSD * cotacao
 
-            print(f"Jogo: {nome} || Preço: {preco}")
+            dealId = jogo['cheapestDealID']
+            linkId = f"https://www.cheapshark.com/redirect?dealID={dealId}"
+
+            print(f"Jogo: {nome} || Preço: {precoBRL}")
             print("=============")
 
             embed = discord.Embed(
-                title=nome, description=preco, color=discord.Color.blue()
+                title=nome, description=f"Menor preço encontrado: **R${precoBRL:.2f}** (**${precoUSD:.2f}**)"
+                , color=discord.Color.dark_blue(), url=linkId
             )
 
             embed.set_image(url=jogo['thumb'])
             await ctx.send(embed=embed)
-
-            await ctx.send(f"{nome}\nMenor preço encontrado: {preco}\n")
     else:
         await ctx.send("Jogo não encontrado.")
         
