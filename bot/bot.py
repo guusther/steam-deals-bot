6e2c343a6b26df20957978ee58ac719f7cdcd5c5
gusbigc <gustavo.ribeiro@acad.ufsm.br>
2026-09-17 03:46:41 -0300

    primeiro teste da api

--- bot/bot.py
+++ bot/bot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
+import aiohttp
 
 #pega o token no .env
 load_dotenv()
@@ -25,16 +26,33 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'O bot {bot.user.name} está online.')
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    if message.content.lower() == "oi":
-        await message.channel.send(f"Olá, {message.author.name}")
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('Pong!')
+
+@bot.command()
+async def batman(ctx):
+    await ctx.send("Buscando dados do Batman...")
+
+    url = "https://www.cheapshark.com/api/1.0/games?title=batman"
+
+    email = os.getenv("USER_EMAIL")
+    autentificador = {'User-Agent': 'SteamDealsBot/1.0 ({email})'}
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=autentificador) as resposta:
+            dados = await resposta.json()
+
+    print("Resposta da API:", dados)
+
+    if isinstance(dados,list) and len(dados) > 0:
+        primeiro_jogo = dados[0]
+        nome = primeiro_jogo['external']
+        preco = primeiro_jogo['cheapest']
+
+        await ctx.send(f"{nome}\n Menor preço encontrado: {preco}")
+    else:
+        await ctx.send("Não foi possível encontrar o jogo.")
 
 bot.run(TOKEN)
