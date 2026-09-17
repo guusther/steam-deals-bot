1720bd8677c94ac737dd7a59e94aa63902c23bb0
gusbigc <gustavo.ribeiro@acad.ufsm.br>
2026-09-17 02:51:56 -0300

    .

--- bot/bot.py
+++ bot/bot.py
@@ -3,20 +3,38 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
+#pega o token no .env
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+#ativa o pacote padrao de eventos do dc. basicamente diz ao server que o bot quer
+#receber maioria das notificações comuns
 intents = discord.Intents.default()
+
+#ativa uma privileged intent chamada conteudo da mensagem, fazendo com que o bot 
+#consiga ler o texto escrito pelos usuários. eh necessario ter isso pra ele ler 
+#comandos como '!ajuda', por exemplo.
 intents.message_content = True
 
+
+#cria o bot e define que ele vai responder a comandos com prefixo '!'
 bot = commands.Bot(command_prefix='!', intents=intents)
+
 
 @bot.event
 async def on_ready():
     print(f'O bot {bot.user.name} está online.')
 
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.lower() == "oi":
+        await message.channel.send(f"Olá, {message.author.name}")
+
 @bot.command()
 async def ping(ctx):
-    await ctx.send('Pong! Estou funcionando perfeitamente. 🏓')
+    await ctx.send('Pong!')
 
 bot.run(TOKEN)
